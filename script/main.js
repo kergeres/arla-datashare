9e60676bbffc4e28bb6dfabdc5c20aba37dfa9b5
kergeres <55786735+kergeres@users.noreply.github.com>
2020-11-05 12:21:12 +0100

    5e2

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
-function string_validate (id_comes_in)
+function string_validate (input)
 {
-   let input = document.querySelector(".input-field");
-  
+
+   // let input = document.querySelector(".input-field");
    let inputval = input.value;
 
 
@@ -36,6 +36,8 @@ function appendFirst ()
 {
    
    let htmltemplate = `  
+   <div class="bg-input-container">
+   <div class="input-container">
    <img src="img/progress1.svg" class="processbar"></img>
    
    <h1>first first first</h1>
@@ -45,62 +47,73 @@ function appendFirst ()
    <div class="navigate-btn-container">
        <button type="submit" class="back">back</button>
        <button type="submit" onclick="appendSecond()" class=" next">next</button>
-   </div>`;
+   </div>
+   </div>
+   </div>`
+   ;
 
-document.querySelector(".input-container").innerHTML = htmltemplate;
+document.querySelector(".input-container-big").innerHTML = htmltemplate;
 
 }
 
 function appendSecond ()
 {
    
-   let htmltemplate = `  
+   let htmltemplate = ` 
+   <div class="bg-input-container">
+   <div class="input-container"> 
    <img src="img/progress2.svg" class="processbar"></img>
    <div class="input-small-container">
        <h4>Kg dry matter per cow</h4>
        <p>Enter the amount of feed consumption per cow.</p>
-       <input id="ifirst" class="input-field" placeholder="test" onkeyup="string_validate(input-field)">
+       <input id="ifirst" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
 
     <div class="input-small-container">
        <h4>Kg milk pr year-cow</h4>
        <p>Enter the amount of the milk production in kg for the current year.</p>
-       <input id="isecond" class="input-field" placeholder="test" onkeyup="string_validate(input-field)">
+       <input id="isecond" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
     <div class="input-small-container">
        <h4>Nr. of cows</h4>
        <p>Enter the amount of cows you have right now.</p>
-       <input id="ithird" class="input-field" placeholder="test" onkeyup="string_validate()">
+       <input id="ithird" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
 
 
    <div class="navigate-btn-container">
        <button type="submit" onclick="appendFirst()" class="back">back</button>
        <button type="submit" onclick="appendThird()" class=" next">next</button>
-   </div>`;
+   </div>
+   </div>
+   </div>`
+   ;
 
-document.querySelector(".input-container").innerHTML = htmltemplate;
+document.querySelector(".input-container-big").innerHTML = htmltemplate;
 
 }
 function appendThird ()
 {
     
-   let htmltemplate = `  <img src="img/progress3.svg" class="processbar"></img>
+   let htmltemplate = `  
+   <div class="bg-input-container">
+   <div class="input-container">
+   <img src="img/progress3.svg" class="processbar"></img>
    <div class="input-small-container">
        <h4>Diesel litre</h4>
        <p>Enter the amount of fuel used for whole farm.</p>
-       <input id="ifourth" class="input-field" placeholder="test" onkeyup="string_validate()">
+       <input id="ifourth" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
 
     <div class="input-small-container">
        <h4>Electricity kWh</h4>
        <p>Enter the amount of electricity you have used for your farm.</p>
-       <input  id="ififth" class="input-field" placeholder="test" onkeyup="string_validate()">
+       <input  id="ififth" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
     <div class="input-small-container">
        <h4>Self-sufficiency in feed</h4>
        <p>% of dry matter</p>
-       <input id="isixth" class="input-field" placeholder="test" onkeyup="string_validate()">
+       <input id="isixth" class="input-field" placeholder="test" onkeyup="string_validate(this)">
    </div>
 
 
@@ -109,9 +122,12 @@ function appendThird ()
    <div class="navigate-btn-container">
        <button type="submit"  onclick="appendSecond()"class="back">back</button>
        <button type="submit" onclick="appendFourth()" class=" next">next</button>
-   </div>`;
+   </div>
+   </div>
+   </div>
+   `;
 
-document.querySelector(".input-container").innerHTML = htmltemplate;
+document.querySelector(".input-container-big").innerHTML = htmltemplate;
 console.log(one, two, three);
 }
 
@@ -123,6 +139,8 @@ function appendFourth ()
    console.log(four, five, six);
    
    let data = `
+   <div class="bg-input-container">
+   <div class="input-container">
    <img src="img/progress4.svg" class="processbar"></img>
    <table>
                     <tbody>
@@ -155,8 +173,11 @@ function appendFourth ()
                 <div class="navigate-btn-container">
                   <button type="submit"  onclick="appendThird()" class="back">Change</button>
                   <button type="submit" onclick="appendFourth()" class=" next">Send In</button>
-               </div>`;
-                document.querySelector(".input-container").innerHTML = data;
+               </div>
+               </div>
+               </div>
+               `;
+                document.querySelector(".input-container-big").innerHTML = data;
 }
 
 
